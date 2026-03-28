be8825bd57b882dd3cd4b02a3f6019f3e165413d
GalileoGalilei960 <haladiyostap@gmail.com>
2026-03-28 09:59:49 +0200

    chore: refactor src working tree

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
     imports: [
