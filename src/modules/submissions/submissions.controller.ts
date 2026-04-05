1371eb19c0a66e05c4fcffc326b0b5e21ac6408a
GalileoGalilei960 <haladiyostap@gmail.com>
2026-04-05 16:16:47 +0300

    feat(submissions): implement CRUD operations for submissions with DTOs and service

---
+++ src/modules/submissions/submissions.controller.ts
@@ -0,0 +1,46 @@
+import {
+    Controller,
+    Get,
+    Post,
+    Body,
+    Patch,
+    Param,
+    Delete,
+    ParseIntPipe,
+} from '@nestjs/common';
+import { SubmissionsService } from './submissions.service';
+import { CreateSubmissionDto } from './dto/create-submission.dto';
+import { UpdateSubmissionDto } from './dto/update-submission.dto';
+
+@Controller('submissions')
+export class SubmissionsController {
+    constructor(private readonly submissionsService: SubmissionsService) {}
+
+    @Post()
+    create(@Body() createSubmissionDto: CreateSubmissionDto) {
+        return this.submissionsService.create(createSubmissionDto);
+    }
+
+    @Get()
+    findAll() {
+        return this.submissionsService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id', ParseIntPipe) id: number) {
+        return this.submissionsService.findOne(id);
+    }
+
+    @Patch(':id')
+    update(
+        @Param('id', ParseIntPipe) id: number,
+        @Body() updateSubmissionDto: UpdateSubmissionDto,
+    ) {
+        return this.submissionsService.update(id, updateSubmissionDto);
+    }
+
+    @Delete(':id')
+    remove(@Param('id', ParseIntPipe) id: number) {
+        return this.submissionsService.remove(id);
+    }
+}
